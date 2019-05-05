17709e2202bd793f2d52a39aec01d2a260a4e992
David A. Lane <kg4giy@gmail.com>
2019-05-05 14:56:44 -0400

    Additional support files.

---
+++ FileStatus.java
@@ -0,0 +1,74 @@
+// Define package code
+
+package com.github.kubernauts.prometheus_example.springboot.instrumented;
+
+//import required packages
+
+import java.io.IOException;
+import java.io.File;
+import java.util.Date;
+import io.prometheus.client.*
+
+// Prometheus counter package.
+import io.prometheus.client.Counter;
+// Prometheus Histogram package.
+import io.prometheus.client.Histogram;
+//Main applicaiton class
+
+public class FileStatus {
+    public static void main(String[] argv) throws IOException {
+
+        // Ensure that a filename (or something) was given in argv[0]
+        if (argv.length == 0) {
+            System.err.println("Usage: FileStatus filename");
+            System.exit(1);
+        }
+        for (String a : argv) {
+            status(a);
+        }
+    }
+
+    public static void status(String fileName) throws IOException {
+        System.out.println("---" + fileName + "---");
+
+        // Construct a File object for the given file.
+        File f = new File(fileName);
+
+        // See if it actually exists
+        if (!f.exists()) {
+            System.out.println("file not found");
+            System.out.println();    // Blank line
+            return;
+        }
+        // Print full name
+        System.out.println("Canonical name " + f.getCanonicalPath());
+        // Print parent directory if possible
+        String p = f.getParent();
+        if (p != null) {
+            System.out.println("Parent directory: " + p);
+        }
+        // Check if the file is readable
+        if (f.canRead()) {
+            System.out.println("File is readable.");
+        }
+        // Check if the file is writable
+        if (f.canWrite()) {
+            System.out.println("File is writable.");
+        }
+        // Report on the modification time.
+        Date d = new Date(f.lastModified());
+        System.out.println("Last modified " + d);
+
+        // See if file, directory, or other. If file, print size.
+        if (f.isFile()) {
+            // Report on the file's size
+            System.out.println("File size is " + f.length() + " bytes.");
+        } else if (f.isDirectory()) {
+            System.out.println("It's a directory");
+        } else {
+            System.out.println("I dunno! Neither a file nor a directory!");
+        }
+
+        System.out.println();    // blank line between entries
+    }
+}
